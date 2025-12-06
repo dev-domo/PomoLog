463cdf253d559c22db644379f6c59e571ec77d67
heoseungjun <gjtmdwns0815@naver.com>
2025-12-06 22:07:58 +0900

    Initial Commit

---
+++ PomoLog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PomoLog
+//
+//  Created by APPLE on 12/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, universe!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PomoLog/PomoLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomoLogApp.swift
+//  PomoLog
+//
+//  Created by APPLE on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct PomoLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
