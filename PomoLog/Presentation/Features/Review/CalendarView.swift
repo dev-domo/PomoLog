a8dfa7a72e5c35bc7352bcc358aa05744f03d640
heoseungjun <gjtmdwns0815@naver.com>
2025-12-24 20:43:38 +0900

    refactor: #11 캘린더 그리드 뷰 분리

--- PomoLog/Presentation/Features/Review/CalendarView.swift
+++ PomoLog/Presentation/Features/Review/CalendarView.swift
@@ -20,7 +20,7 @@ struct CalendarView: View {
             HStack {
                 VStack {
                     CalendarHeaderView(month: $month)
-                    calendarGridView
+                    CalendarGridView(month: $month, clickedDate: $clickedDate)
                 }
                 
                 TimelineView(clickedDate: $clickedDate)
@@ -29,72 +29,31 @@ struct CalendarView: View {
         .gesture(
             DragGesture()
                 .onChanged { gesture in
-                    self.offset = gesture.translation
+                    initOffset(gesture.translation)
                 }
                 .onEnded { gesture in
-                    if gesture.translation.width < -100 {
-                        changeMonth(by: 1)
-                    } else if gesture.translation.width > 100 {
-                        changeMonth(by: -1)
+                    let width = gesture.translation.width
+                    
+                    if width < dragToLeft {
+                        changeMonth(by: .next)
+                    } else if width > dragToRight {
+                        changeMonth(by: .previous)
                     }
-                    self.offset = CGSize()
+                    initOffset()
                 }
         )
     }
-    
-    // MARK: - 날짜 그리드 뷰
-    private var calendarGridView: some View {
-        let daysInMonth: Int = numberOfDays(in: month)
-        let firstWeekday: Int = firstWeekdayOfMonth(in: month) - 1
-        
-        return VStack {
-            LazyVGrid(columns: Array(repeating: GridItem(), count: 7)) {
-                ForEach(0 ..< daysInMonth + firstWeekday, id: \.self) { index in
-                    if index < firstWeekday {
-                        RoundedRectangle(cornerRadius: 5)
-                            .foregroundColor(Color.clear)
-                    } else {
-                        let date = getDate(for: index - firstWeekday)
-                        let day = index - firstWeekday + 1
-                        let clicked = clickedDate == date
-                        
-                        CalendarCellView(day: day, clicked: clicked)
-                            .onTapGesture {
-                                clickedDate = date
-                            }
-                    }
-                }
-            }
-        }
-    }
 }
 
-// MARK: - 내부 메서드
 private extension CalendarView {
     
-    private func getDate(for day: Int) -> Date {
-        return Calendar.current.date(byAdding: .day, value: day, to: startOfMonth())!
+    private func initOffset(_ offset: CGSize = CGSize()) {
+        self.offset = offset
     }
     
-    func startOfMonth() -> Date {
-        let components = Calendar.current.dateComponents([.year, .month], from: month)
-        return Calendar.current.date(from: components)!
-    }
-    
-    func numberOfDays(in date: Date) -> Int {
-        return Calendar.current.range(of: .day, in: .month, for: date)?.count ?? 0
-    }
-    
-    func firstWeekdayOfMonth(in date: Date) -> Int {
-        let components = Calendar.current.dateComponents([.year, .month], from: date)
-        let firstDayOfMonth = Calendar.current.date(from: components)!
-        
-        return Calendar.current.component(.weekday, from: firstDayOfMonth)
-    }
-    
-    func changeMonth(by value: Int) {
+    private func changeMonth(by moveMonth: MoveMonth) {
         let calendar = Calendar.current
-        if let newMonth = calendar.date(byAdding: .month, value: value, to: month) {
+        if let newMonth = calendar.date(byAdding: .month, value: moveMonth.rawValue, to: month) {
             self.month = newMonth
         }
     }
