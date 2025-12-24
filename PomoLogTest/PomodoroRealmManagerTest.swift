dcf774c840e22952357d72f85f20959b66243d10
heoseungjun <gjtmdwns0815@naver.com>
2025-12-24 20:41:30 +0900

    test: #11 특정 날짜에 포모도로 기록이 있는지 확인하는 테스트 작성

--- PomoLogTest/PomodoroRealmManagerTest.swift
+++ PomoLogTest/PomodoroRealmManagerTest.swift
@@ -15,13 +15,34 @@ struct PomodoroRealmManagerTest {
     
     @Test("날짜를 기준으로 포모도로 기록 조회")
     func fetchByDate_pomodoro__success() throws {
-        let realm = InMemoryDatabaseFactory.createRealm()
-        let realmManager = PomodoroRealmManager(realm: realm)
-        let pomodoroModel = PomodoroModel()
+        let realmManager = createPomodoroRealmManager()
+        let pomodoroModel = createPomodoroModel()
         let _ = realmManager.save(model: pomodoroModel)
         
         let fetchedData = try #require(realmManager.fetchByDate(target: Date()))
         
         #expect(fetchedData.first == pomodoroModel)
+    }
+    
+    @Test("오늘 날짜에 포모도로 기록이 있는지 여부 조회")
+    func hasPomodoro__result() throws {
+        let realmManager = createPomodoroRealmManager()
+        let pomodoroModel = createPomodoroModel()
+        let _ = realmManager.save(model: pomodoroModel)
+        
+        let hasPomodoro = try #require(realmManager.hasPomodoro(target: Date()))
+        
+        #expect(hasPomodoro)
+    }
+    
+    private func createPomodoroRealmManager() -> PomodoroRealmManager {
+        let realm = InMemoryDatabaseFactory.createRealm()
+        let realmManager = PomodoroRealmManager(realm: realm)
+        
+        return realmManager
+    }
+    
+    private func createPomodoroModel() -> PomodoroModel {
+        return .init()
     }
 }
